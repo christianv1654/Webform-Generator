8f34677bcbb41021e4e0438ebc9ac73d2b3f91ad
Selvetta <fernandomoreno010@gmail.com>
2023-05-24 00:17:51 -0700

    Package Json Changes

--- webform-gen/app/page.tsx
+++ webform-gen/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import NavBar from '@/components/NavBar'
 
 export default function Home() {
   return (
     <div>
       <h1> This is the home page</h1>
+      <NavBar />
     </div>
   )
 }
---
+++ webform-gen/pages/Login.tsx
@@ -0,0 +1,15 @@
+export default function Login() {
+
+
+
+
+
+    return (
+        <div>
+            <h1>This is the login Page</h1>
+        </div>
+
+    )
+}
+
+
